8d9fbab23273930bebfa0cec011c46e3642a534d
MennaYaserSetait <menna.setait@gmail.com>
2024-02-20 16:55:50 +0200

    First Commit
    printing

---
+++ TEST/TEST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TEST
+//
+//  Created by Menna Setait on 20/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(" hello,first line ")
+    }
+
+
+}
+
